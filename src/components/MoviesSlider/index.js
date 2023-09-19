d8e52592e3e397d2a28217ae4dadc6ca654fb47e
Shashank <shashankmende@gmail.com>
2023-09-19 05:49:24 +0000

    second commit

--- src/components/MoviesSlider/index.js
+++ src/components/MoviesSlider/index.js
@@ -1,19 +1,21 @@
 // Write your code here
 import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import MovieItem from '../MovieItem'
 import './index.css'
 
 const MovieSlider = props => {
   const {actionMovies} = props
   const settings = {
-    dots: true,
-    slidesToShow: 1,
+    dots: false,
+    slidesToShow: 4,
     slidesToScroll: 1,
   }
 
   return (
     <div className="slider-container" data-testid="movie">
-      <Slider>
+      <Slider {...settings}>
         <div>
           {actionMovies.map(each => (
             <MovieItem key={each.id} movie={each} />
